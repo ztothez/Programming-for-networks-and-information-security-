c6b43e4eafc2bbf21dfdfc4b9a91c467adb37d09
ztothez <34552429+ztothez@users.noreply.github.com>
2019-10-31 22:44:27 +0200

    1.2.6: File Access Finished
    Did the last exercise of file access modules

--- exercises/07_file-access_actvity.py
+++ exercises/07_file-access_actvity.py
@@ -1,8 +1,12 @@
 #Toni Tuunainen 1.2.6.4: Activity – Create a Script to Allow User to Add Devices
 devices=[]
-file=open("devices.txt","r")
-for item in file:
-    item=item.strip()
-    devices.append(item)
-file.close()
-print(devices)
+file=open("devices.txt","a")
+while True:
+    newItem=input("Enter device name: ")
+    if newItem == 'exit' :
+        file.close()
+        print("All done!")
+        break
+    while True:
+        file.write(newItem + "\n")
+        break
