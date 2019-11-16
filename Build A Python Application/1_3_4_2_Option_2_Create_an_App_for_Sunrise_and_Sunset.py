f94b63cf831810b208eb4419d2628a4886413143
ztothez <34552429+ztothez@users.noreply.github.com>
2019-11-16 02:42:58 +0200

    FIle Changes

--- Build A Python Application/1_3_4_2_Option_2_Create_an_App_for_Sunrise_and_Sunset.py
+++ Build A Python Application/1_3_4_2_Option_2_Create_an_App_for_Sunrise_and_Sunset.py
@@ -2,8 +2,22 @@
 
 import urllib.parse
 import requests
+from datetime import datetime, timedelta
+import time
+import pgeocode
 
-url = "https://api.sunrise-sunset.org/json?lat=48.8584&lng=2.2945"
-
+def timeconv(time,name):
+    in_time = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S%z")
+    localtime = in_time+timedelta(hours=timezone)
+    out_time = localtime.strftime("%H:%M:%S")
+    print(name+": "+out_time)
+    
+country = input("Add country code: ")
+postalcode = input("Add postalcode: ")
+nomi = pgeocode.Nominatim(country)
+lists = nomi.query_postal_code(postalcode).tolist()
+url = "https://api.sunrise-sunset.org/json?lat="+str(lists[9])+"&lng="+str(lists[10])+"&formatted=0"
+timezone = int(time.strftime("%z").replace("0","").replace("+",""))
 json_data = requests.get(url).json()
-print(json_data)
+timeconv(json_data["results"]["sunrise"],"sunrise")
+timeconv(json_data["results"]["sunset"],"sunset")
--- Build A Python Application/iss.py
+++ Build A Python Application/iss.py
@@ -1,4 +1,3 @@
-#Toni Tuunainen 1.3.4.3: Option 3 - Create an App for the ISS
 #!/bin/python3
 
 import json
