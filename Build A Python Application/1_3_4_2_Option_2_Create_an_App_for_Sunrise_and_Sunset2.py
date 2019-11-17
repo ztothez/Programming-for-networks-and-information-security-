22ef9be7771c33035c5a32cc414ae6df8cfcc926
ztothez <34552429+ztothez@users.noreply.github.com>
2019-11-17 13:58:27 +0200

    Delete 1_3_4_2_Option_2_Create_an_App_for_Sunrise_and_Sunset2.py

--- Build A Python Application/1_3_4_2_Option_2_Create_an_App_for_Sunrise_and_Sunset2.py
+++
@@ -1,8 +0,0 @@
-#Toni Tuunainen 1.3.3.8: Activity - Test Status and URL Print Commands
-
-import urllib.parse
-import requests
-
-main_api = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&formatted=0"
-json_data = requests.get(main_api).json()
-print(json_data)
